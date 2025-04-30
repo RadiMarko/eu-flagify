1b1e0968e1fef84a0b8517c42a7a4f08a63ef540
RadiMarko <r.marko1995@gmail.com>
2025-04-30 16:34:56 +0200

    Add star ratings to score modal

--- src/Components/ResultModal.jsx
+++ src/Components/ResultModal.jsx
@@ -2,9 +2,32 @@ export default function ResultModal(props) {
     
     let scorePercentage = Math.round((props.score / 54) * 1000) / 10;
     
+    const starArray = [
+        <span style={{fontSize:"400%", color:"yellow"}}>★</span>,
+        <span style={{fontSize:"400%", color:"yellow"}}>★★</span>,
+        <span style={{fontSize:"400%", color:"yellow"}}>★★★</span>,
+        <span style={{fontSize:"400%", color:"yellow"}}>★★★★</span>,
+        <span style={{fontSize:"400%", color:"yellow"}}>★★★★★</span>
+    ]
+    
+    function getStars(score) {
+        if (score >= 50) {
+            return starArray[4];
+        } else if (score >= 40) {
+            return starArray[3];
+        } else if (score >= 30) {
+            return starArray[2];
+        } else if (score >= 20) {
+            return starArray[1];
+        } else if (score < 20) {
+            return starArray[0];
+        }
+    }
+    
     return (
         <div className={props.modalShown ? "result-modal-shown" : "result-modal-hidden"}>
-            <h2>Final score: {props.score} ({scorePercentage}%)</h2>
+            <h2 className="result-details">Final score: {props.score} flags ({scorePercentage}%)</h2>
+            <span>{getStars(props.score)}</span>
             <button 
                 className="button-on" 
                 style={{width: "10rem", height: "4.2rem", fontSize: "1.5rem"}}
